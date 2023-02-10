46b886e9b7834a451b8b693ecb4e7c3782fb3624
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-10 17:30:02 -0600

    adjusted dashboard and auth layouts

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,20 @@
-import '@/styles/global.css'
 import GlassPane from "@/components/GlassPane";
+import "@/styles/global.css";
+import { Inter } from "@next/font/google";
 
-export default function AuthRootLayout({children}) {
-    return (
-        <html lang="en">
-            <head />
-            <body className="h-screen w-screen rainbow-mesh p-6">
-                <GlassPane className="w-full h-full flex items-center justify-center">
-                  {children}
-                </GlassPane>
-            </body>
-        </html>
-    )
+const inter = Inter({
+  variable: "--font-inter",
+});
+
+export default function AuthRootLayout({ children }) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,22 @@
-import '@/styles/global.css'
+import "@/styles/global.css";
+import { Inter } from "@next/font/google";
+import clsx from "clsx";
 import GlassPane from "@/components/GlassPane";
 
-export default function DashboardRootLayout({children}) {
-    return (
-        <html lang="en">
-            <head />
-            <body className="h-screen w-screen rainbow-mesh p-6">
-                <GlassPane className="w-full h-full flex items-center justify-center">
-                  {children}
-                </GlassPane>
-            </body>
-        </html>
-    )
+const inter = Inter({
+  variable: "--font-inter",
+});
+
+export default function DashboardRootLayout({ children }) {
+  return (
+    <html lang="en" className={clsx(inter.variable, "dark")}>
+      <head />
+      <body className="h-screen w-screen candy-mesh p-6">
+        <GlassPane className="w-full h-full p-6 flex align-center container mx-auto">
+            
+          <main className="w-full pl-6 h-full">{children}</main>
+        </GlassPane>
+      </body>
+    </html>
+  );
 }
