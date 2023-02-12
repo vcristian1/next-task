9cff4783a1ed2f21526508f520c8e91bb24d8d7a
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-12 13:36:30 -0600

    copied HomePageLoader and added it to the project page directory so the loader renders when you click a project

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -29,13 +29,13 @@ export default async function Page() {
   const { projects } = await getData()
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
-      <div className=" h-full  items-stretch justify-center min-h-[content]">
+      <div className="h-full items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
           <Suspense fallback={<GreetingsSkeleton />}>
             <Greetings />
           </Suspense>
           </div>
-        <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
+        <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3">
           {projects.map((project) => (
               <div className="w-1/3 p-3" key={project.id}>
                 <Link href={`/project/${project.id}`}>
