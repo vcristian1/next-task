007b7fcd85caca5f7e2f42b7c9d5e85268647fe1
Cristian Vargas <cristian.v0223@gmail.com>
2023-02-12 11:55:14 -0600

    created GreetingsSkeleton component which functions as our greeting loader, and addedused suspense to wait for the greetings component to load in home page.tsx

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,4 +1,5 @@
 import Greetings from "@/components/Greetings";
+import GreetingsSkeleton from "@/components/GreetingsSkeleton";
 import { delay } from "@/lib/async";
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -10,7 +11,11 @@ export default async function Page() {
   return (
     <div className="h-full overflow-y-auto pr-6 w-1/1">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
-        <div className="flex-1 grow flex"><Greetings /></div>
+        <div className="flex-1 grow flex">
+          <Suspense fallback={<GreetingsSkeleton />}>
+            <Greetings />
+          </Suspense>
+          </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** projects map here */}
           <div className="w-1/3 p-3">{/* new project here */}</div>
